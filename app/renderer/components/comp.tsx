b0c4ddd272f6c97faf220b01d6f08ba6b34d5666
miloas <0x9357@gmail.com>
2018-04-05 13:33:51 +0800

    upgrade dep

--- app/renderer/components/comp.tsx
+++ app/renderer/components/comp.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
-const styles = {
-  App: {
-    marginTop: 100,
-    textAlign: 'center'
-  }
+const App: React.CSSProperties = {
+  marginTop: 100,
+  textAlign: 'center'
 }
 
 export class Comp extends React.PureComponent {
   public render() {
     return (
-      <div style={ styles.App }>
+      <div style={ App }>
         <h1>World</h1>
         <h3><Link to='/'>Back</Link></h3>
       </div>
